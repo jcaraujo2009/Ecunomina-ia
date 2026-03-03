89b3ed09f6e9329bce673948c3c4ea1742e20d09
jcaraujo <jcaraujo@tiempolibre.ec>
2026-03-03 09:21:48 -0500

    fix middleware to use next-auth/middleware for Edge Runtime

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
-import NextAuth from 'next-auth';
+import { withAuth } from 'next-auth/middleware';
 import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig).auth;
+export default withAuth(authConfig);
 
 export const config = {
-    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
